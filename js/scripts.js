55065de28aa382c82be0a4f954500868cf25da49
Oscar Maritim <oscarmaritim3@gmail.com>
2019-05-30 11:49:15 +0300

    created a funtion to determine isoceles triangle

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,13 @@ function scaleneTriangle (num1, num2, num3){
  console.log ("it is not a scalene triangle");
  }
 }
-
+function isocelesTriangle (num1, num2, num3){
+  if ((num1 == num2 && num2 !== num3) || (num1 !== num2 && num2 == num3)){
+    console.log("it is an isosceles triangle");
+  }else {
+    console.log("It is not an isoceles trianle");
+  }
+}
 var num = [3];
 num[0]= prompt("Enter the first side");
 num[1] = prompt("Enter the second number");
