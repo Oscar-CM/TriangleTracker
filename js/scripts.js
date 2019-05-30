5ac3289679ee136679623d0dbe26a8fb9f07c126
Oscar Maritim <oscarmaritim3@gmail.com>
2019-05-30 11:48:24 +0300

    created a funtions to determine scalene triangles

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+function scaleneTriangle (num1, num2, num3){
+ if(num1 !== num2 && num1!==num3 && num1 !== num3){
+ console.log("It is a scalene triangle");
+ } else{
+ console.log ("it is not a scalene triangle");
+ }
+}
+
+var num = [3];
+num[0]= prompt("Enter the first side");
+num[1] = prompt("Enter the second number");
+num[2] = prompt ("Enter the third number");
+isocelesTriangle(num[0], num[1], num[2]);
+scaleneTriangle(num[0],num[1],num[2]);
