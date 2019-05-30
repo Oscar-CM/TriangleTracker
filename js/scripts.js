11fdf2a60d906ac215d4d872614ed400405b9437
Oscar Maritim <oscarmaritim3@gmail.com>
2019-05-30 11:53:16 +0300

    adding a function for equilateral triangle

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,21 @@ function isocelesTriangle (num1, num2, num3){
   if ((num1 == num2 && num2 !== num3) || (num1 !== num2 && num2 == num3)){
     console.log("it is an isosceles triangle");
   }else {
-    console.log("It is not an isoceles trianle");
+    console.log("It is not an isoceles triangle");
+  }
+}
+function equilateralTriangle (num1, num2, num3){
+  if (num1 == num2 && num2 == num3){
+    console.log("It is an equilateral triangle");
+  }else{
+    console.log("It is not an equilateral triangle");
   }
 }
 var num = [3];
 num[0]= prompt("Enter the first side");
 num[1] = prompt("Enter the second number");
 num[2] = prompt ("Enter the third number");
+
 isocelesTriangle(num[0], num[1], num[2]);
 scaleneTriangle(num[0],num[1],num[2]);
+equilateralTriangle(num[0], num[1], num[2]);
