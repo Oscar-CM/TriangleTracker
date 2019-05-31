600ef564a00bac23a954ab96901c299fadb0759e
Oscar Maritim <oscarmaritim3@gmail.com>
2019-05-31 15:21:19 +0300

    Adding the link to the script in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 
+function determineIfTriangle(num1, num2, num3){
+  if((num1+num2 > num3) && (num1 + num3 > num2) && (num2 + num3 > num1)){
+    console.log("it is a triangle");
+  } else {
+    console.log("it is not a triangle");
+  }
+}
 function scaleneTriangle (num1, num2, num3){
  if(num1 !== num2 && num1!==num3 && num1 !== num3){
  console.log("It is a scalene triangle");
@@ -20,10 +27,20 @@ function equilateralTriangle (num1, num2, num3){
     console.log("It is not an equilateral triangle");
   }
 }
+function myFunction() {
+  document.getElementById("form-group").reset();
+    }
+function results(){
+  document.getElementById("output").innerHTML = "Paragraph changed!";
+  }
 var num = [3];
-num[0]= prompt("Enter the first side");
-num[1] = prompt("Enter the second number");
-num[2] = prompt ("Enter the third number");
+num[0] = document.getElementById("num1").value;
+num[1] = document.getElementById("num2").value;
+num[2] = document.getElementById("num3").value;
+submitOK = "true";
+
+
+
 
 isocelesTriangle(num[0], num[1], num[2]);
 scaleneTriangle(num[0],num[1],num[2]);
