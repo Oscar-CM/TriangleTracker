f229fa1eedaa2c685affcf08c0d686e79db126d1
Oscar Maritim <oscarmaritim3@gmail.com>
2019-06-02 10:36:01 +0300

    successfully linked script.js to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,30 @@
+function  results(){
+ 
 
-function determineIfTriangle(num1, num2, num3){
-  if((num1+num2 > num3) && (num1 + num3 > num2) && (num2 + num3 > num1)){
-    console.log("it is a triangle");
-  } else {
-    console.log("it is not a triangle");
-  }
-}
-function scaleneTriangle (num1, num2, num3){
- if(num1 !== num2 && num1!==num3 && num1 !== num3){
- console.log("It is a scalene triangle");
- } else{
- console.log ("it is not a scalene triangle");
- }
-}
-function isocelesTriangle (num1, num2, num3){
-  if ((num1 == num2 && num2 !== num3) || (num1 !== num2 && num2 == num3)){
-    console.log("it is an isosceles triangle");
-  }else {
-    console.log("It is not an isoceles triangle");
-  }
-}
-function equilateralTriangle (num1, num2, num3){
-  if (num1 == num2 && num2 == num3){
-    console.log("It is an equilateral triangle");
-  }else{
-    console.log("It is not an equilateral triangle");
-  }
-}
-function myFunction() {
-  document.getElementById("form-group").reset();
+  var num1= parseInt(document.getElementById("sideA").value);
+  var num2=parseInt(document.getElementById("sideB").value);
+  var num3=parseInt(document.getElementById("sideC").value);
+ 
+  var sides = [num1, num2, num3];
+  
+
+    if(sides[0] <=0 || sides[1] <=0 || sides[2] <=0){
+      document.getElementById("output").innerHTML="it is not a triangle";
     }
-function results(){
-  document.getElementById("output").innerHTML = "Paragraph changed!";
+     else if(sides[0]+sides[1]<sides[2] || sides[1]+sides[2]<sides[0] && sides[2]+sides[0]<sides[1]){
+      document.getElementById("output").innerHTML="Does not make a triangle";
+    }
+    else if(sides[0]===sides[1] && sides[1]===sides[2] && sides[2]===sides[0]){ 
+      document.getElementById("output").innerHTML= " Equilateral Triangle";
+    }
+    else if(sides[0]===sides[1] || sides[0]===sides[2] || sides[1]===sides[2]){
+      document.getElementById("output").innerHTML="Iscosceles Triangle";
+    }
+    else if(sides[0]!==sides[1] && sides[0]!==sides[2] && sides[2]!==sides[1] && sides[0]+sides[1]>sides[2] && sides[1]+sides[2]>sides[0] && sides[2]+sides[0]>sides[1]){
+      document.getElementById("output").innerHTML="scalene triangle";
+    }
+   
+    else {
+      document.getElementById("output").innerHTML="Enter valid values";
+    }
   }
-var num = [3];
-num[0] = document.getElementById("num1").value;
-num[1] = document.getElementById("num2").value;
-num[2] = document.getElementById("num3").value;
-submitOK = "true";
-
-
-
-
-isocelesTriangle(num[0], num[1], num[2]);
-scaleneTriangle(num[0],num[1],num[2]);
-equilateralTriangle(num[0], num[1], num[2]);
