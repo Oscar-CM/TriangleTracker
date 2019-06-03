6354e8b062de4246d40876e5146714d9de4abdb1
Oscar Maritim <oscarmaritim3@gmail.com>
2019-06-03 09:39:35 +0300

    Ensured proper alignment in my html

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 function  results(){
- 
+
 
   var num1= parseInt(document.getElementById("numOne").value);
   var num2=parseInt(document.getElementById("numTwo").value);
   var num3=parseInt(document.getElementById("numThree").value);
- 
+
   var sides = [num1, num2, num3];
-  
+
+  document.getElementById("output").innerHTML="It is not a triangle";
+
 
     if(sides[0] <=0 || sides[1] <=0 || sides[2] <=0){
       document.getElementById("output").innerHTML="It is not a triangle";
@@ -14,7 +16,7 @@ function  results(){
     else if(sides[0]+sides[1]<sides[2] || sides[1]+sides[2]<sides[0] || sides[2]+sides[0]<sides[1]){
       document.getElementById("output").innerHTML="Does not make a triangle";
     }
-    else if(sides[0]===sides[1] && sides[1]===sides[2] && sides[2]===sides[0]){ 
+    else if(sides[0]===sides[1] && sides[1]===sides[2] && sides[2]===sides[0]){
       document.getElementById("output").innerHTML= " Equilateral Triangle";
     }
     else if(sides[0]===sides[1] || sides[0]===sides[2] || sides[1]===sides[2]){
@@ -23,7 +25,7 @@ function  results(){
     else if(sides[0]!==sides[1] && sides[0]!==sides[2] && sides[2]!==sides[1] && sides[0]+sides[1]>sides[2] && sides[1]+sides[2]>sides[0] && sides[2]+sides[0]>sides[1]){
       document.getElementById("output").innerHTML="Scalene Triangle";
     }
-   
+
     else {
       document.getElementById("output").innerHTML="Enter valid values";
     }
